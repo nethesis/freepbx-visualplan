ce4390c4f9190e3f02ae9dcdcf535936382a6625
Stefano Fancello <gentoo.stefano@gmail.com>
2024-03-22 18:11:09 +0100

    Fix announcement creation on NS8

--- htdocs/create.php
+++ htdocs/create.php
@@ -423,18 +423,18 @@ function nethvplan_switchCreate($wType, $value, $connectionArray)
 
             if (!array_key_exists($value['id'], $currentCreated)) {
                 if (empty($id)) {
-                    $idAnn = announcement_add($name, $rec_id, "", $destination, null, null, null);
+                    $idAnn = FreePBX::Announcement()->addAnnouncement($name, $rec_id, "", $destination, 0, 0, "");
                     $idReturn = $idAnn;
                     $currentCreated[$value['id']] = $idReturn;
                     $destinations = nethvplan_getDestination($value, $connectionArray);
                     $destination = trim($destinations["output_".$value['entities'][2]['id']]);
-                    announcement_edit($idReturn, $name, $rec_id, "", $destination, null, null, null);
+                    FreePBX::Announcement()->editAnnouncement($idReturn, $name, $rec_id, "", $destination, 0, 0, "");
                 } else {
                     $currentCreated[$value['id']] = $id;
                     $destinations = nethvplan_getDestination($value, $connectionArray);
                     $destination = trim($destinations["output_".$value['entities'][2]['id']]);
                     $exists = announcement_get($id);
-                    announcement_edit($id, $name, $rec_id, $exists['allow_skip'], $destination, $exists['return_ivr'], $exists['noanswer'], $exists['repeat_msg']);
+                    FreePBX::Announcement()->editAnnouncement($id, $name, $rec_id, $exists['allow_skip'], $destination, $exists['return_ivr'], $exists['noanswer'], $exists['repeat_msg']);
                     $idReturn = $id;
                 }
             }
